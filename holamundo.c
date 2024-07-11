b6dd5d18eecc6486e6d0af7948625aeb4a36a775
VisualVillan <152357865+VisualVillan@users.noreply.github.com>
2024-07-11 22:51:44 +0000

    Burbuja corregido

--- holamundo.c
+++ holamundo.c
@@ -12,7 +12,7 @@ void ordenamiento_burbuja(int arr[], int n)
 {
     for(int i = 0; i < n-1; i++){
         for(int j = 0; i <n-i-1; j++){
-            printf("Comparando %d con %d \n", arr[j]; arr[j+1]);
+            printf("Comparando %d con %d \n", arr[j], arr[j+1]);
             if(arr[j] > arr[j+1]){
                 intercambiar(&arr[j], &arr[j+1]);
             }
