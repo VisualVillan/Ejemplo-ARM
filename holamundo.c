560d4dae7672e152b3e20b5d3c58708b14106896
VisualVillan <152357865+VisualVillan@users.noreply.github.com>
2024-07-11 22:18:58 +0000

    Algoritmo Burbuja

--- holamundo.c
+++ holamundo.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main()
+void intercambiar(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void ordenamiento_burbuja(int arr[], int n)
 {
-    printf("tercera version \n");
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; i <n-i-1; j++){
+            printf("Comparando %d con %d \n", arr[j]; arr[j+1]);
+            if(arr[j] > arr[j+1]){
+                intercambiar(&arr[j], &arr[j+1]);
+            }
+        }
 
+    }
+
+}
+
+int main(){
+    srand(time(NULL));
     return 0;
 }
