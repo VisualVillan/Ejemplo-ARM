bb4cbdf03b8bdad515f4cd1d96e19c0890e63faa
VisualVillan <152357865+VisualVillan@users.noreply.github.com>
2024-07-11 22:25:47 +0000

    Algoritmo de insercion

--- holamundo.c
+++ holamundo.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main()
-{
-    printf("tercera version \n");
+void ordenamiento_insercion(int arr[], int n){
+    for(int i = 1; i < n; i++){
+        int valor_actual = arr[];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > valor_actual){
+            printf("Comparando &d con %d \n", arr[j], valor_actual);
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j+1] = valor_actual;
+    }
+}
 
+int main(){
+    srand(time(NULL));
     return 0;
 }
